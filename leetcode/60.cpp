1c3035fad66dde3a8635dc65ef8d39b71721137a
pthaike <pthaike@gmail.com>
2017-01-17 23:06:27 +0800

    update

---
+++ leetcode/60.cpp
@@ -0,0 +1,15 @@
+/**
+https://leetcode.com/problems/permutation-sequence/
+The set [1,2,3,бн,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order,
+We get the following sequence (ie, for n = 3):
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+*/
