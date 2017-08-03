db06747ad67c3a1122607e71e8b5f3fdc3fa720c
pthaike <pating@MININT-OT9N72J.fareast.corp.microsoft.com>
2017-08-03 20:25:05 +0800

    add leetcode 401

---
+++ leetcode/401.cpp
@@ -0,0 +1,20 @@
+/**
+https://leetcode.com/problems/binary-watch/description/
+
+*/
+class Solution {
+public:
+    vector<string> readBinaryWatch(int num) {
+        vector<string> ret;
+        for(int h = 0; h < 12; h++)
+        {
+            for(int m = 0; m < 60; m++)
+            {
+                if(bitset<10>(h<<6 | m).count() == num)
+                    ret.push_back(to_string(h) + (m < 10 ? ":0" : ":") + to_string(m));
+            }
+        }
+        return ret;
+    }
+    
+};
